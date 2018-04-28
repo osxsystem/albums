c57a428f67c9fc11f31860d97aa1f75d3374461b
Hung Do <osxsystem2014@gmail.com>
2018-04-28 19:55:55 +0700

    get data from local api

--- src/components/albumList.js
+++ src/components/albumList.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
-import { Text, View } from "react-native";
+import { Text, View } from 'react-native';
+import axios from 'axios';
 
-const AlbumList = () => {
-    return (
-        <View>
-            <Text>This is list of albums</Text>
-        </View>
-    );
+class AlbumList extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = { albums: [] };
+    }
+    componentDidMount() {
+        axios.get("http://reactapis.local/api/albums")
+            .then(response => this.setState({ albums: response.data }))
+            .catch(function (error) {
+                console.log(error);
+            }); 
+    }
+
+    renderAlbums() {
+        const { albums } = this.state;
+        console.log(albums.data);
+        if (typeof albums.data !== "undefined") {
+            return albums.data.map(album => 
+            <Text key={album.id}>{album.title}</Text>
+        );
+        }
+    }
+
+    render() {
+        return (
+            <View>
+                {this.renderAlbums()}
+            </View>
+        );
+    }
 }
 
 export default AlbumList;
